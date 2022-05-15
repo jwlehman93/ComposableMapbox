335705b08478f8733373f0c26e3ec4cb3eeeac77
Jeremy Lehman <jeremy.lehman@onxmaps.com>
2022-05-14 17:30:28 -0700

    Add initial xcode project

---
+++ Shared/ComposableMapboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableMapboxApp.swift
+//  Shared
+//
+//  Created by Jeremy Lehman on 5/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ComposableMapboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
